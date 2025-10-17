7063261c7a79fccf544360ea38db59f67c64bfd9
Alexandre Reyes Mengual <24470180@campeche.tecnm.mx>
2025-10-17 16:42:00 -0600

    tercer commit donde muestra el pograma ya completo

--- Calculadora de propinas/MainPage.xaml.cs
+++ Calculadora de propinas/MainPage.xaml.cs
@@ -2,13 +2,57 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        decimal propina;
+        decimal propinaPersona;
+        decimal total;
+        decimal TotalPersona;
         public MainPage()
         {
             InitializeComponent();
+            Limpiar();
+
+
+        }
+    
+        public void Cacular_Button(object sender, EventArgs e)
+        {
+            decimal consumo = Convert.ToDecimal(ConsumoEntry.Text);
+            decimal NumPersonas = Convert.ToDecimal(NumPersonaEntry.Text);
+            decimal porcentaje = Convert.ToDecimal(PropinaEntry.Text);
+            propina = consumo * (porcentaje / 100);
+            propinaPersona = propina / NumPersonas;
+            total = consumo + propina;
+            TotalPersona = total / NumPersonas;
+            Mostrar();
+
+
         }
 
-        
+        private void Limpiar_Button(object sender, EventArgs e)
+        {
+            Limpiar();
+
+        }
+        public void Mostrar()
+        {
+
+            ImportePropinaOUT.Text = $"${propina:F2}";
+            PersonaPropinaOUT.Text = $"${propinaPersona:F2}";
+            ImporteTotalOUT.Text = $"${total:F2}";
+            TotalOUT.Text = $"${TotalPersona:F2}";
+        }
+        public void Limpiar()
+        {
+            ImportePropinaOUT.Text = "$0.00";
+            PersonaPropinaOUT.Text = "$0.00";
+            ImporteTotalOUT.Text = "$0.00";
+            TotalOUT.Text = "$0.00";
+
+
+        }
     }
+
+
+
 }
+
